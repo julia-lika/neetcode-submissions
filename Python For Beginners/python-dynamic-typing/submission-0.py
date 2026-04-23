8c1adb3fe79a9f5a1a1491c9a52959e63f2a7ea2
Júlia Lika Ishikawa <159062938+julia-lika@users.noreply.github.com>
2026-04-22 22:02:45 -0300

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 15
+print(type(variable))
+variable = 15.5
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Oloko"
+print(type(variable))
+variable = [1, 3, "pipoca"]
+print(type(variable))
